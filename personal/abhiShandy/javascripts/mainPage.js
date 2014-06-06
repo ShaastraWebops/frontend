a975d4c197c9bc1786c4f3c141463c8119d10bb3
abhiShandy <abhishek.sandy@gmail.com>
2014-06-07 01:01:32 +0530

    Finetuning home page

--- personal/abhiShandy/javascripts/mainPage.js
+++ personal/abhiShandy/javascripts/mainPage.js
@@ -18,7 +18,7 @@ var cAnimate = function(id){
 	var pathLength = x.getTotalLength();
 	x.style.strokeDashoffset = pathLength;
 	x.style.strokeDasharray = pathLength+" "+pathLength;
-	var duration = 10;
+	var duration = 8;
 	var delay = 1;
 	x.style.WebkitAnimation = "cubeAnimate "+duration+"s ease-in-out "+delay+"s forwards";
 }
@@ -36,3 +36,54 @@ tAnimate("sR");
 tAnimate("sT");
 cAnimate("cubeBorder");
 cAnimate("cubeDots");
+
+$(document).ready(function(){
+	$(".menu_options").hover(function(){
+		$(this).css("background-color","#EBF4FA");
+		$(this).css("color","black");
+		$("#facebook").css("color","#3b5998");    
+		$("#twitter").css("color","#428bca"); 
+		$("#you_tube").css("color","#cc181e");
+		$("#close_menu").css("color","#99C68E"); 
+		$(this).css("font-weight","bold"); 
+	},function(){
+		$(this).css("background-color","black"); 
+		$(this).css("color","white"); 
+		$("#facebook").css("color","#3b5998");
+		$("#twitter").css("color","#428bca"); 
+		$("#you_tube").css("color","#cc181e");
+		$(this).css("font-weight","normal"); 		
+		$("#close_menu").css("color","#99C68E");    
+	});
+	$("#logoSVG").click(function(){
+	    $("#menu_popup").animate({      
+	      left: '0%'
+	    },"slow");  
+	    $("#menu_rhs").animate({
+	      right: '0%',
+	      width: '70%'
+	    },"slow");  
+  	});
+	//replace it with css for better design
+	$("#close_menu").click(function(){  
+		$("#menu_popup").animate({
+			left: '-30%'
+		},"slow");                      
+		$("#menu_rhs").animate({
+		    right: '-70%',
+		    width: '0%'
+  		},"slow");
+	});
+
+	fOpacity=0.4;
+	$("#lectures").hover(function(){
+    	$("#mikeSVG").css("opacity",fOpacity);
+  	},function(){
+    	$("#mikeSVG").css("opacity","0");
+  	});
+  	$("#events").hover(function(){
+    	$("#toolSVG").css("opacity",fOpacity);
+	},function(){
+    	$("#toolSVG").css("opacity","0");
+  	});
+});
