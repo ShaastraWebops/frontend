f48fa0f273903d61ed927dfd99dacb8e7ecda209
chinnichaitanya <chchaitanya95@gmail.com>
2014-06-09 01:26:19 +0530

    total_upto_contacts

--- personal/chaitanya/js/contacts_js.js
+++ personal/chaitanya/js/contacts_js.js
@@ -4,26 +4,63 @@ $(document).ready(function(){
     },function(){
       $(this).css("background-color","");        
    });   
-});    
+    
 
-$(function() {  
-    var txt = $('#comments'),  
-        hiddenDiv = $(document.createElement('div')),  
-        content = null;  
+    $(function() {  
+        var txt = $('#comments'),  
+            hiddenDiv = $(document.createElement('div')),  
+            content = null;  
   
-    txt.addClass('txtstuff');  
-    hiddenDiv.addClass('hiddendiv common');  
+        txt.addClass('txtstuff');  
+        hiddenDiv.addClass('hiddendiv common');  
   
-    $('body').append(hiddenDiv);  
+        $('body').append(hiddenDiv);  
   
-    txt.on('keyup', function () {  
+        txt.on('keyup', function () {  
   
-        content = $(this).val();  
+            content = $(this).val();  
   
-        content = content.replace(/\n/g, '<br>');  
-        hiddenDiv.html(content + '<br class="lbr">');  
+            content = content.replace(/\n/g, '<br>');  
+            hiddenDiv.html(content + '<br class="lbr">');  
   
-        $(this).css('height', hiddenDiv.height());  
+            $(this).css('height', hiddenDiv.height());  
   
-    }); 
+        }); 
+    });
+    
+    
+    $("#co-curricular_affairs").click(function(){
+       $("#div").load("contacts_click.html #target_logo", function() {
+        $("#details").css("background","url('../pics/co-curricular_affairs.png') no-repeat top center");
+       }); 
+//        $("#div").append($("<div>").load("contacts_click.html #evolve"));        
+    });
+    $("#pr_spons_networking").click(function(){
+       $("#div").load("contacts_click.html #target_logo", function() {
+        $("#details").css("background","url('../pics/pr_spons_etc.png') no-repeat top center");
+       }); 
+    });
+    $("#events").click(function(){
+       $("#div").load("contacts_click.html #target_logo", function() {
+        $("#details").css("background","url('../pics/events.png') no-repeat top center");
+       }); 
+    });
+    $("#sr").click(function(){
+       $("#div").load("contacts_click.html #target_logo", function() {
+        $("#details").css("background","url('../pics/evolve.png') no-repeat top center");   
+       }); 
+    });    
 });  
+
+
+
+
+
+
+
+
+
+
+
+
+
