ba195a8bde8ac8a1c17097b7b4c3745c72272a0a
abhiShandy <abhishek.sandy@gmail.com>
2014-06-19 00:23:47 +0530

    footer added

--- projects/minimal_mainsite/php/foot.php
+++ projects/minimal_mainsite/php/foot.php
@@ -1,3 +1,4 @@
+<footer id="footer" style='position:absolute; bottom:1%'> &copy; 2015 Shaastra Webops </footer>
 <!-- Javascript -->
 <script src="../bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript">
@@ -13,6 +14,8 @@ $(document).ready(function(){
 	function redirectPage () {
 		window.location = linkLocation;
 	}
+	x = document.getElementById("footer");
+	x.style.left = (1-(x.clientWidth/window.innerWidth))*50+"%";
 });
 </script>
     <!--<script src="../js/jquery_migrate.js"></script>-->
