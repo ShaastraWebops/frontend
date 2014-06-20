ba59693bd77b6c97586ed255e2d68940cd1b7b6c
Deepak Padamata <deepakpadamata@gmail.com>
2014-06-20 17:29:22 +0530

    landing page added

---
+++ projects/minimal_mainsite/php/landing.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="../css/flipclock.css">
+		<?php include 'head.php' ?>
+		<style type="text/css">
+			body { 
+				  background: url(../img/landing/background.jpg) no-repeat center center fixed; 
+				  -webkit-background-size: cover;
+				  -moz-background-size: cover;
+				  -o-background-size: cover;
+				  background-size: cover;
+				}
+			img {
+				display: block;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			#logo{
+				margin-top: 2%;
+				width: 60%;
+			}
+			#tools{
+				margin-top: 1%;
+				width: 20%;
+			}
+			p {
+	    		font-family: 'Titillium Web', sans-serif;
+	    		font-size: 1.15em;
+	    		text-align: center;
+	    		color: black;
+			}
+			.your-clock{
+				margin-left: auto;
+				margin-right: auto;
+				width: 35%;
+			}
+		</style>
+	</head>
+	<body>
+		<img id="logo" src="../img/landing/logo.png">
+		<img id="tools" src="../img/landing/tools.png">
+		<p>We will be live in</p>
+		<br>
+		<div class="your-clock"></div>
+		
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/flipclock.min.js"></script>
+		<script type="text/javascript">
+			var clock = new FlipClock($('.your-clock'), {
+			});
+			clock.setCountdown(true);
+			clock.setTime(3600);
+		</script>
+	</body>
+</html>
