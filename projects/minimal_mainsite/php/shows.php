f126ac484cbdfccdf9e6090bf85a0d12a77d4ee1
AbdealiJK <abdealikothari@gmail.com>
2014-06-25 09:23:44 +0530

    writeup mods shows.php

--- projects/minimal_mainsite/php/shows.php
+++ projects/minimal_mainsite/php/shows.php
@@ -50,6 +50,9 @@
                     <br />
                     Being the only student organized event of its kind in the Whole Country, We proudly bet you will never ever forget your journey into Neverland, giving a new meaning to your idea of entertainment and turning projects undertaken by us into a memory you will long cherish! 
                     <br />
+                    The work of science can be enigmatic. Have your sight deceived by animations shown using just a strip of LEDs. You will know which boxes to blame if you are unable to get sleep that night. Get ready to witness people around you floating in air. After you have a taste of the Virtual Music and Video Mixing we offer, DJs will seem old school. Ever wanted to take a selfie with a superhero, this might well be the chance. You will witness a new groundbreaking performance after watching our techno cultural show.
+                   <br />
+                   
                 </div>
                 <div class="col-md-6">
                     <iframe style="margin: 10px;" width="100%" height="315" src="http://www.youtube.com/embed/Ci5i6Ali07I" frameborder="0" allowfullscreen align="right"></iframe>
@@ -57,8 +60,6 @@
             </div>
             <div class="row">
                 <div class="col-md-12">
-                The work of science can be enigmatic. Have your sight deceived by animations shown using just a strip of LEDs. You will know which boxes to blame if you are unable to get sleep that night. Get ready to witness people around you floating in air. After you have a taste of the Virtual Music and Video Mixing we offer, DJs will seem old school. Ever wanted to take a selfie with a superhero, this might well be the chance. You will witness a new groundbreaking performance after watching our techno cultural show.
-                <br />
                 Last year, we achieved a Limca World Record for the largest Holographic Projection and The First ever Virtual Band. Like a seagull skimming the waves of entertainment over the ocean of imagination, with the technical dexterity of IIT-M students to give wings to our dream, we are set to roar into the blues of success. Join us at Shaastra 2015 to share the awe and experience the Dream Kingdom!
                 <br />
                 </div>
