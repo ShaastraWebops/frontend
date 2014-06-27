354eaa02d2b9683616fb0fa2cbe9bb4efc69c59a
abhiShandy <abhishek.sandy@gmail.com>
2014-06-27 12:36:45 +0530

    trying 3D

--- personal/abhiShandy/js/lecturesMOS.js
+++ personal/abhiShandy/js/lecturesMOS.js
@@ -6,3 +6,12 @@ else{
 	localStorage.visitCount = 1;
 }
 console.log("No. of visits: "+localStorage.visitCount);
+
+function pageCenterPercent (id, z) {
+	x = document.getElementById(id);
+	x.style.position = "absolute";
+	x.style.zIndex = z;
+	x.style.top = (1-(x.clientHeight/window.innerHeight))*50+"%";	
+	x.style.left = (1-(x.clientWidth/window.innerWidth))*50+"%";
+}
+pageCenterPercent("opening", 2);
