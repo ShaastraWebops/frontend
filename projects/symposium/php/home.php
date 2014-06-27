dab8af04a875827d9fa9e9b6abc7766e400db849
ifilemymass <kunalkishore.mail@gmail.com>
2014-06-27 23:54:11 +0530

    separated navbar.php

--- projects/symposium/php/home.php
+++ projects/symposium/php/home.php
@@ -28,35 +28,7 @@
 	</script>-->
 	
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand menu">Symposium 2015</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="default" id="home"><a href="">Home</a></li>
-			<li class="default" id="about"><a href="">About</a></li>
-            <li class="default" id="schedule"><a href="">Schedule</a></li>
-            <li class="default" id="speakers"><a href="">Speakers</a></li>
-            <li class="default" id="partners"><a href="">Partners</a></li>
-			<li class="default" id="faq"><a href="">FAQ</a></li>
-			<li class="default" id="register"><a href="">Register</a></li>
-			<li class="default" id="hospitality"><a href="">Hospitality</a></li>
-			<li class="default" id="contact"><a href="">Contact</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
-        </div>
-      </div>
-    </div>
+    <?php include 'navbar.php' ?>
 	
 	<?php include 'sidebar.php' ?>	
 	
