8df56ca7aea1c58033f44f5b05f6a9b3485688a4
abhiShandy <abhishek.sandy@gmail.com>
2014-06-30 14:01:10 +0530

    div animation, only 5 #40

--- personal/abhiShandy/js/lecturesMOS.js
+++ personal/abhiShandy/js/lecturesMOS.js
@@ -9,9 +9,15 @@ console.log("No. of visits: "+localStorage.visitCount);
 
 function pageCenterPercent (id, z) {
 	x = document.getElementById(id);
-	x.style.position = "absolute";
+	x.style.position = "fixed";
 	x.style.zIndex = z;
 	x.style.top = (1-(x.clientHeight/window.innerHeight))*50+"%";	
 	x.style.left = (1-(x.clientWidth/window.innerWidth))*50+"%";
 }
-pageCenterPercent("opening", 2);
+// pageCenterPercent("opening", 2);
+sHeight = window.innerHeight;
+document.getElementById("p1").style.height = (sHeight/2)+"px";
+document.getElementById("p2").style.height = (sHeight/2)+"px";
+document.getElementById("p3").style.height = (sHeight/2)+"px";
+document.getElementById("p4").style.height = (sHeight/2)+"px";
+document.getElementById("p5").style.height = (sHeight/2)+"px";
