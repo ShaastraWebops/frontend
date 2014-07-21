51231773986b1df6cebed5150a5f7639437b4ff7
AbdealiJK <abdealikothari@gmail.com>
2014-07-21 16:35:28 +0530

    Update foot.php

--- projects/main/php/base/foot.php
+++ projects/main/php/base/foot.php
@@ -16,3 +16,24 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/modernizr/2.0.6/modernizr.min.js' type='text/javascript'></script>
 <?php } ?>
+
+<script>
+
+	$(document).ready(function() 
+	{
+	$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+	})
+</script>
