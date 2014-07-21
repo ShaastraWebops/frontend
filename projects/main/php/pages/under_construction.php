38a8786c170bf4cede69fb44b193761c4996ef66
AbdealiJK <abdealikothari@gmail.com>
2014-07-21 16:02:03 +0530

    Update under_construction.php

--- projects/main/php/pages/under_construction.php
+++ projects/main/php/pages/under_construction.php
@@ -1,1 +1,49 @@
-d
+<html>
+	<head>
+		<link rel="shortcut icon" href="../../img/favicon.ico">
+
+		<title>Under Construction | Shaastra 2015</title>
+		<?php include '../base/head.php' ?>
+		
+		<style type="text/css">
+			body { 
+				background: -webkit-radial-gradient(#fff, #ddd); /* For Safari 5.1 to 6.0 */
+				background: -o-radial-gradient(#fff, b#ddd); /* For Opera 11.1 to 12.0 */
+				background: -moz-radial-gradient(#fff, #ddd); /* For Firefox 3.6 to 15 */
+				background: radial-gradient(#fff, #ddd); /* Standard syntax */
+			}
+			img {
+				display: block;
+				margin: auto;
+			}
+			#logo{
+				margin-top: 2%;
+				width: 60%;
+			}
+			#tools{
+				margin-top: 1%;
+				width: 20%;
+			}
+			p {
+	    		font-size: 1.15em;
+	    		text-align: center;
+	    		color: black;
+			}
+		</style>
+	</head>
+	<body>
+		<img id="logo" src="../../img/logo/1000x250_dark.png">
+		<img id="tools" src="../../img/misc/tools.png">
+			
+		<br>
+		<p>
+			Woah There! This page has not yet been made. <br />
+			Some one's probably making it right now !<br /><br />
+			Come back and check later.
+		</p>
+		
+		<?php include '../base/foot.php' ?>
+		<script src="../../js/flipclock.min.js"></script>
+		
+	</body>
+</html>
