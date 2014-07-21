40d181f6be7a921916a41cdacdc353ac253bfd2e
AbdealiJK <abdealikothari@gmail.com>
2014-07-21 18:08:02 +0530

    Update phpfuncs.php

--- projects/main/php/base/phpfuncs.php
+++ projects/main/php/base/phpfuncs.php
@@ -1,45 +1,26 @@
 <?php
 
-/**
- * Modifies a string to remove all non ASCII characters and spaces.
- */
-function slugify($text)
-{
-    // replace non letter or digits by -
+function slugify($text) {
     $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
- 
-    // trim
     $text = trim($text, '-');
- 
-    // transliterate
-    if (function_exists('iconv'))
-    {
+    if (function_exists('iconv')) {
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
     }
- 
-    // lowercase
     $text = strtolower($text);
- 
-    // remove unwanted characters
     $text = preg_replace('~[^-\w]+~', '', $text);
  
-    if (empty($text))
-    {
+    if (empty($text)) {
         return 'n-a';
     }
- 
     return $text;
 }
  
  
- function endsWith($haystack, $needle)
-{
+function endsWith($haystack, $needle) {
     return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
 }
 
-function show404()
-{
-    
+function show404() {
     include '../pages/404.php';
     die();
 }
