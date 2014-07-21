e3100a8aa8c56f588e9d26d39a38d01a7959ff4b
AbdealiJK <abdealikothari@gmail.com>
2014-07-21 16:32:56 +0530

    Update eventlist.php

--- projects/main/php/pages/eventlist.php
+++ projects/main/php/pages/eventlist.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Events | Shaastra '15</title>
+    <?php 
+		if ( isset($_REQUEST['category']) ) {
+			$category = $_REQUEST['category'];
+			$category_path = '../events/' . $category ;
+		}
+                if (!isset($category) || $category=="") { // No category, send back to categories page
+                    header('Location: ../pages/categories.php');  
+                }
+                
+                if (!is_dir($category_path)) {
+		    header('Location: ../pages/categories.php'); 
+		} else {
+		    $event_list = scandir($category_path);
+		}
+                
+	?>
+    <title><?php echo $category; ?> Events | Shaastra '15</title>
     
     <?php include '../base/head.php' ?>
     <style>
@@ -45,28 +61,31 @@
             letter-spacing: 0.15vw; 
             text-transform: uppercase;
         }
-        #event-list .event-group .event-item .transparent-text {
-            background: #fff;
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-            -moz-background-clip: text;
-            -moz-text-fill-color: transparent;
-            -ms-background-clip: text;
-            -ms-text-fill-color: transparent;
-            -o-background-clip: text;
-            -o-text-fill-color: transparent;
-            background-clip: text;
-            text-fill-color: transparent;
-            -webkit-text-stroke-color: white;
-            -webkit-text-stroke-width: 0.2px;
-            -moz-text-stroke-color: white;
-            -moz-text-stroke-width: 0.2px;
-            -ms-text-stroke-color: white;
-            -ms-text-stroke-width: 0.2px;
-            -o-text-stroke-color: white;
-            -o-text-stroke-width: 0.2px;
-            text-stroke-color: white;
-            text-stroke-width: 0.2px;
+        @media screen and (-webkit-min-device-pixel-ratio:0) { /* Is only done in Chrome and Safari */
+            #event-list .event-group .event-item .transparent-text {
+                background: #fff;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                background-clip: text;
+                text-fill-color: transparent;
+                -webkit-text-stroke-color: white;
+                -webkit-text-stroke-width: 0.2px;
+                text-stroke-color: white;
+                text-stroke-width: 0.2px;
+            }
+        }
+        @-moz-document url-prefix() { /* Is only done in Firefox */
+            #event-list .event-group .event-item .transparent-text {
+                background: #fff;
+                -moz-background-clip: text;
+                -moz-text-fill-color: transparent;
+                background-clip: text;
+                text-fill-color: transparent;
+                -moz-text-stroke-color: white;
+                -moz-text-stroke-width: 0.2px;
+                text-stroke-color: white;
+                text-stroke-width: 0.2px;
+            }
         }
         #event-list .event-group .event-item a div {
             position: relative;
@@ -109,6 +128,10 @@
             margin-top: 0%;
             margin-bottom: 10%;
         }
+        #event-list .event-group.first.last .event-item a div {
+            margin-top: 5%;
+            margin-bottom: 5%;
+        }
         #event-list .event-group .event-item.first a div {
             margin-left: 10%;
             margin-right: 0%;
@@ -116,6 +139,10 @@
         #event-list .event-group .event-item.last a div {
             margin-left: 0%;
             margin-right: 10%;
+        }
+        #event-list .event-group .event-item.first.last a div {
+            margin-top: 5%;
+            margin-bottom: 5%;
         }
 
         #event-list .event-group .event-item a:hover div {
@@ -131,20 +158,27 @@
         #event-list .event-group.last .event-item a:hover div {
             margin-top: -100%;
         }
+        #event-list .event-group.first.last .event-item a:hover div {
+            margin-top: auto;
+        }
         #event-list .event-group .event-item.first a:hover div {
             margin-left: 100%;
         }
         #event-list .event-group .event-item.last a:hover div {
             margin-left: -100%;
         }
+        #event-list .event-group .event-item.first.last a:hover div {
+            margin-left: auto;
+        }
     </style>
 </head>
 <body>
+        
     <?php include '../base/menu.php' ?>
     <div class="container-fluid white centered">
             <div class="row">
                 <div class="col-xs-12">
-                    <h1 class="text-center title">EVENTS</h1>
+                    <h1 class="text-center title">EVENTS - <?php echo $category; ?></h1>
                     <div class="white breaker">
                         <span class="left"></span>
                         <div class="dice white"></div>
@@ -155,150 +189,46 @@
         </div>
 
     <div id="event-list" class="container-fluid">
-        <div class="row event-group first">
-            <div class="col-md-2 col-md-offset-1 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg first">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
+        <?php
+            $event_row_count = 5;
+            $event_count = count($event_list);
+            $event_i = 0;
+            foreach ( $event_list as $event ) {
+                if ($event === '.' or $event === '..') continue;
+        ?>
+            <?php if ( $event_i % $event_row_count == 0 ) { ?>
+                <div class="row event-group <?php // Give different class to indicate first and last row
+                    if ($event_i == 0) echo 'first';
+                    elseif (floor($event_i/$event_row_count) == floor($event_count/$event_row_count)-1) echo 'last'; 
+                ?>">
+            <?php } ?>
+                    <div class="col-md-2 col-md-offset-<?php // Give a padding for first event-item
+                        if ($event_i % $event_row_count == 0) echo '1';
+                        else echo '0'; 
+                    ?> col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg <?php // Give different class to indicate first and last column
+                        if ($event_i % $event_row_count == 0) echo 'first';
+                        elseif ($event_i % $event_row_count == $event_row_count-1) echo 'last'; 
+                    ?>">
                         <div>
-                            <span>
-                                <span class="transparent-text"> 
-                                    Hackfest
-                                </span>
-                            </span>
+                            <div class="dummy"></div>
+                            <a href="../pages/event.php?category=<?php echo urlencode($category); ?>&event=<?php echo urlencode($event); ?>">
+                                <div>
+                                    <span>
+                                        <span class="transparent-text"> 
+                                            <?php echo $event; ?>
+                                        </span>
+                                    </span>
+                                </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span class="transparent-text">
-                                    Robowars
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span class="transparent-text">
-                                    Boeing National Aero modelling Competition
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span class="transparent-text">
-                                    Mobile Dev workshop
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg last">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span class="transparent-text">
-                                    Open Programing Contest
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="row event-group last">
-            <div class="col-md-2 col-md-offset-1 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg first">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span>
-                                    HELLO
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span>
-                                    HELLO
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span>
-                                    HELLO
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span>
-                                    HELLO
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-3 event-item event-bg last">
-                <div>
-                    <div class="dummy"></div>
-                    <a href="../pages/event.php?category=Aerofest&event=Airshow&tab=Introductions">
-                        <div>
-                            <span>
-                                <span>
-                                    HELLO
-                                </span>
-                            </span>
-                        </div>
-                    </a>
-                </div> 
-            </div>
-        </div>
+                    </div> <!-- End a event-item -->
+            <?php if($event_i % $event_row_count == $event_row_count-1) { ?>
+                </div> <!-- End a event-group -->
+            <?php } ?>
+        <?php
+                $event_i++;
+            }
+        ?>
     </div>
     <?php include '../base/foot.php' ?>
 </body>
