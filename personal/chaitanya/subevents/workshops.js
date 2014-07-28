3d4fae1148419d455993a8e6a945aafa37000b3e
chinnichaitanya <chchaitanya95@gmail.com>
2014-07-28 17:06:57 +0530

    ...

--- personal/chaitanya/subevents/workshops.js
+++ personal/chaitanya/subevents/workshops.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
     
    $("#workshops_button").click(function() {
-      $("#workshops_div").css("display","initial"); 
+        $("#workshops_div").css("display","initial"); 
+    setInterval(function() {
+        $("#spanner").toggleClass("rotate");
+        $("#nut").toggleClass("rotate_nut");   
+
+    }, 1000);
+
    });
-    
+        
 });
+
+
