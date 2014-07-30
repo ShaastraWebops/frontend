89e49d63d03ce12f2aa16f7c637bd5f0fb4ffe73
chinnichaitanya <chchaitanya95@gmail.com>
2014-07-30 17:14:45 +0530

    update

--- personal/chaitanya/subevents/workshops.js
+++ personal/chaitanya/subevents/workshops.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-    
-   $("#workshops_button").click(function() {
-        $("#workshops_div").css("display","initial"); 
-    setInterval(function() {
-        $("#spanner").toggleClass("rotate");
-        $("#nut").toggleClass("rotate_nut");   
-
-    }, 1000);
-
+$(document).ready(function() {  
+   i = 1; 
+   $('#workshops_button').click(function() {
+        $('#workshops_load').animate({'width': '100%'}, 1800);   
+        time1 = setInterval(function() {
+                $('#spanner').toggleClass('rotate');
+            }, 1000);
+        time2 = setInterval(function() {
+                t = 60*i;           
+                $('#nut').css({
+                    '-webkit-transform': 'rotate(' + t + 'deg)',
+                    '-webkit-transition-duration': '1s',
+                    '-webkit-transition-delay': '1s',
+                    '-webkit-animation-timing-function': 'linear',
+                    '-webkit-transform-origin': '50% 50% 0'
+                }); 
+                i += 1;   
+            }, 2000);
+        $('#spanner')
+            .delay(4100)
+            .queue( function(next){ 
+            $(this).css('display','initial'); 
+            next(); 
+        });          
+        $('#nut')
+            .delay(2100)
+            .queue( function(next){ 
+            $(this).css('display','initial'); 
+            next(); 
+        });          
    });
-        
 });
-
-
