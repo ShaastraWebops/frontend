f3c1b385b01434cefdc782412d3a8529f9bd3c00
Chiranjeevi <chiru6753@gmail.com>
2014-08-16 15:56:20 +0530

    lite

--- projects/main/php/scripts/create_file.php
+++ projects/main/php/scripts/create_file.php
@@ -1,15 +1,13 @@
 <?php
 $dir = $_REQUEST['dirname'];
 $name = $_REQUEST['newname'];
-
 $filename = $dir . '/' . $name;
 $filenamenew = $filename;
 $i = 1;
 while ( file_exist($filenamenew) ) {
-    $i = $i + 1;
-    $filenamenew = $filename . '($i)';
+$i = $i + 1;
+$filenamenew = $filename . '($i)';
 }
-
 echo fopen($filenamenew . '/html', "w"); // create
 ?>
 
