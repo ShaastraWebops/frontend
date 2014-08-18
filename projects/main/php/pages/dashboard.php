166b9b5df0a3fd593ade10fa433af699580e2860
abhiShandy <abhishek.sandy@gmail.com>
2014-08-19 00:01:54 +0530

    basic layout for dashboard and registration

--- projects/main/php/pages/dashboard.php
+++ projects/main/php/pages/dashboard.php
@@ -3,9 +3,9 @@
     <head>
         <?php
             if (isset($logged_in) && $logged_in) {
-                //header('Location: ../pages/dashboard.php');
+                header('Location: ../pages/dashboard.php');
             } else {
-                header('Location: ../pages/login.php');
+                // header('Location: ../pages/login.php');
             }
         ?>
         <title>Dashboard | Shaatsra '15</title>
@@ -32,10 +32,88 @@
 
         <div class="container-fluid main white">
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                  YOOOOOO
+                <div class="col-md-2 col-md-offset-3">
+                    <img class="img-thumbnail" id="profile-pic" src="../../img/login/photo.jpg" alt="Profile Pic"></img>
+                </div>
+                <div class="col-md-4">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Shaastra ID:</th>
+                                    <th>MM12B037</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Name:</th>
+                                    <td>Abhishek Shandilya</td>
+                                </tr>
+                                <tr>
+                                    <th>Email id:</th>
+                                    <td>abhishek.sandy@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <th>College:</th>
+                                    <td>IIT MADRAS</td>
+                                </tr>
+                                <tr>
+                                    <th>Phone:</th>
+                                    <td>+91 9884299211</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>
+                                        <button class="btn btn-info">Edit Profile</button>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-info">Change your profile pic</button>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
     		</div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Registered Events</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="event.php?category=Aerofest&amp;event=Aerobotics">Aerobotics</a></td>
+                                    <td>TDP</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="event.php?category=Design+and+Build&amp;event=Robotics">Robotics</a></td>
+                                    <td>TDP</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="event.php?category=Coding&amp;event=Reverse+Coding">Reverse Coding</a></td>
+                                    <td>Qualification Round</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>
+                                        <button class="btn btn-info">Register for an event</button>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-warning">Unregister from an event</button>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
     <?php include '../base/foot.php' ?>
