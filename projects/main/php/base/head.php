2c486ccaff4e5edbc992410afa69b3092f706643
Abdeali JK <abdealikothari@gmail.com>
2014-09-03 21:27:55 +0530

    Session start

--- projects/main/php/base/head.php
+++ projects/main/php/base/head.php
@@ -1,5 +1,11 @@
 <?php 
-if(session_status()!=PHP_SESSION_ACTIVE) session_start()
+if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+    if ( session_status() !== PHP_SESSION_ACTIVE )
+    	session_start();
+} else {
+    if ( session_id() === '' )
+    	session_start();
+}
 //session_start();
 $DEBUG = 1;
 
