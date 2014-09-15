40ee90f3cc0f5e8d1c96461ddbf3006636a5b908
SaiPraveen <saipraveenb25@gmail.com>
2014-09-15 22:52:28 +0530

    fixed login.php replaced POST with REQUEST and added optional redirect to dashboard.php

--- projects/main/php/scripts/login.php
+++ projects/main/php/scripts/login.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-if ( !isset($_POST['user_id']) || !isset($_POST['email'])
-	|| !isset($_POST['first_name']) || !isset($_POST['last_name']) ) {
+if ( !isset($_REQUEST['user_id']) || !isset($_REQUEST['email'])
+	|| !isset($_REQUEST['first_name']) || !isset($_REQUEST['last_name']) ) {
 	header('HTTP/1.1 500 Internal Server Error');
 	echo <<<ERRORTEXT
 {'error' : 'invalid input'}
@@ -10,14 +10,16 @@ ERRORTEXT;
 
 }
 
-$_SESSION['user_id'] = $_POST['user_id'];
+$_SESSION['user_id'] = $_REQUEST['user_id'];
 //$_SESSION['shaastra_id'] = $_POST['shaastra_id'];
-$_SESSION['token'] = $_POST['token'];
-$_SESSION['first_name'] = $_POST['first_name'];
-$_SESSION['last_name'] = $_POST['last_name'];
-$_SESSION['email'] = $_POST['email'];
+$_SESSION['token'] = $_REQUEST['token'];
+$_SESSION['first_name'] = $_REQUEST['first_name'];
+$_SESSION['last_name'] = $_REQUEST['last_name'];
+$_SESSION['email'] = $_REQUEST['email'];
 
-//header('Location: ../pages/dashboard.php')
+if( isset($_REQUEST['redirect']) )
+    header('Location: ../pages/dashboard.php');
+
 echo json_encode(array(
 	"success" => "Logged in successfully"
 ));
