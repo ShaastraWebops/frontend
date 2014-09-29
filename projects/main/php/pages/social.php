aa22be45f2e8c8bd7333525eda1871641db41df9
Abdeali JK <abdealikothari@gmail.com>
2014-09-29 15:22:41 +0530

    Edited the social.php page

--- projects/main/php/pages/social.php
+++ projects/main/php/pages/social.php
@@ -37,7 +37,7 @@
 		}
 		</style>
 	</head>
-	
+
 	<body>
 		<?php include '../base/menu.php' ?>
 		<div class="container-fluid white centered">
@@ -61,41 +61,35 @@
     </div>-->
 
 	<style>
-		#feature {
-			text-align: justify;
-			padding-left: 50px;
-			
-		}
-		
 		.lead{
 			font-size: 16px;
 		}
-		#foot{
-			font-size: 16px;
-		}
-  	</style>  
+        .col-centered > p {
+            text-align: justify;
+        }
+  	</style>
   	<style type="text/css" id="holderjs-style"></style>
- 
+
       <!-- START THE FEATURETTES -->
-	<div id="feature">
-		<div class="row featurette">
-			<div class="col-md-4">
-				<img src="../../img/misc/Social/Shaastra-2015-pledge-a-book-1.jpg" class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="500x500">
+	<div id="feature" class="container-fluid">
+		<div class="row row-centered">
+			<div class="col-md-3 col-centered">
+				<img src="../../img/misc/Social/Shaastra-2015-pledge-a-book-1.jpg" class="img-responsive" data-src="holder.js/500x500/auto" alt="500x500">
 			</div>
-			<div class="col-md-7">
+			<div class="col-md-7 col-centered">
 				<h2 class="text-center title">PLEDGE-A-BOOK</h2>
-				<p class="lead"><b>T</b>he Pledge-A-Book campaign is Shaastra's attempt to give back to society and to create impact in the fields of education and literacy. This campaign aims to collect books which are donated by patrons of Shaastra, and use these books to set up libraries for underprivileged students.  The campaign was flagged off by renowned science writer Mr. Anil Ananthaswamy. Since then, there have been several collection drives within the campus, including one at the Terry Fox Run Chennai, all of which have received a great response.  
+				<p class="lead"><b>T</b>he Pledge-A-Book campaign is Shaastra's attempt to give back to society and to create impact in the fields of education and literacy. This campaign aims to collect books which are donated by patrons of Shaastra, and use these books to set up libraries for underprivileged students.  The campaign was flagged off by renowned science writer Mr. Anil Ananthaswamy. Since then, there have been several collection drives within the campus, including one at the Terry Fox Run Chennai, all of which have received a great response.
 				<br><b>S</b>haastra has already confirmed associations with well-known NGO the Indian Development Foundation, and Avanti Fellows, and other associations are in the pipeline. The books collected through the campaign are being used to set up libraries for underprivileged students at IDF's Bal Gurukuls, as well as at Avanti Fellows' Learning Centres.  The Pledge-A-Book campaign is our humble attempt at bringing change and helping design the dreams of the young. We would appreciate your support in this initiative. <i> Join us, and help be the change! </i></p></p>
 			</div>
 		</div>
 
-		<div class="row featurette">			
-			<div class="col-md-7">
+		<div class="row row-centered">
+			<div class="col-md-7 col-centered">
 				<h2 class="text-center title">THE PAST</h2>
 				<p class="lead">Shaastra 2014's motto was "Breaking Boundaries", and to this regard, a number of initiatives were taken up and executed. Among them were movements like Computer Literacy For All, Intellectual Property Rights Awareness Drive, Ideas Challenge and Shaastra Sparsh.  <b>Computer Literacy For All</b>, expanded from its successful 2013 edition, was a movement that aimed at productively dealing with e-waste. The computers collected by the institute were serviced and subsequently distributed in a school in the college's rural neighborhood. <b>The Intellectual Property Rights Awareness Drive</b> was an initiative aimed at spreading awareness about IPR and copyright protection among the youth through fun initiatives such as quizzes and competitions. <br><b>Ideas Challenge</b> was a competition where participants attempted to solve socially-relevant problem statements suggested by NGOs and RuTAG using their technical skills and engineering knowledge. <b>Shaastra Sparsh</b> was a movement that associated with schools in Chennai, aimed at interacting with younger children.  In addition to such wholly independent initiatives, a number of events at Shaastra are themed with relevant social issues in the backdrop. Events which have sustainable development and environment-friendly issues as their central theme are a rapidly evolving genre at Shaastra. For example, Shaastra 2014 had <b>Sustainable Cityscape</b> as a key event. <b>Shaastra Symposium</b>, a key event in any iteration of Shaastra, serves as a platform for rigourous debate on relevant social issues. For example, Shaastra 2013 Symposium discussed "Education and Governance", Shaastra 2014's symposium was on "Agriculture in India". </p>
 			</div>
-			<div class="col-md-4">
-				<img src="../../img/misc/Social/Shaastra-2015-pledge-a-book-2.jpg" class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="500x500">
+			<div class="col-md-4 col-centered">
+				<img src="../../img/misc/Social/Shaastra-2015-pledge-a-book-2.jpg" class="img-responsive" data-src="holder.js/500x500/auto" alt="500x500">
 			</div>
 		</div>
 
@@ -103,9 +97,9 @@
       <!-- /END THE FEATURETTES -->
 
       	<?php include '../base/foot.php' ?>
-		
+
 		<script type="text/javascript">
-			
+
 		</script>
 	</body>
 </html>
