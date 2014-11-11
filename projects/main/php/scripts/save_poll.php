19dbf7c66058d3a6a5b47f031d56276cb7bfd27e
chinnichaitanya <chchaitanya95@gmail.com>
2014-11-11 18:32:34 +0530

    new workshops

--- projects/main/php/scripts/save_poll.php
+++ projects/main/php/scripts/save_poll.php
@@ -1,8 +1,13 @@
 <?php
 $vote = $_REQUEST['vote'];
 date_default_timezone_set("Asia/Kolkata");
-$present_time_date = " | " . date("h:i:sa") . " | " . date("Y-m-d") . " | " . date("l");
-$vote_time = $vote . "," . $present_time_date;
+// $present_time_date = " | " . date("h:i:sa") . " | " . date("Y-m-d") . " | " . date("l");
+// $vote_time = $vote . "," . $present_time_date;
+$time = date("h:i:sa");
+$date = date("Y-m-d");
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$list = array($vote, $time, $date, $ip);
 
 $a = strcmp($vote, "b_events");
 $b = strcmp($vote, "coding");
@@ -18,7 +23,7 @@ if( !$fp ) {
 	echo "<h4> But error occurred while saving-(Type_1) </h4>";
 } else {
 	if($final == 0) {
-		if(fputcsv($fp, str_split($vote_time, strlen($vote_time)))) {
+		if(fputcsv($fp, $list)) {
 			echo "<h4> Thank you for your feedback </h4>";		
 			echo "<h4> Successfully saved ! </h4>";
 			exit;
