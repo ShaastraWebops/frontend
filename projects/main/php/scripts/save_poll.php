a1d7c2c172e58d9fe1854e0683d644445ff9d2b6
chinnichaitanya <chchaitanya95@gmail.com>
2014-11-17 03:35:50 +0530

    new poll

--- projects/main/php/scripts/save_poll.php
+++ projects/main/php/scripts/save_poll.php
@@ -1,20 +1,37 @@
 <?php
 $vote = $_REQUEST['vote'];
-$filename = '../../php/misc/poll.csv';
+date_default_timezone_set("Asia/Kolkata");
+$time = date("h:i:sa");
+$date = date("Y-m-d");
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$list = array($vote, $time, $date, $ip);
+
+$a = strcmp($vote, "Yes");
+$b = strcmp($vote, "No");
+$final = $a*$b;
+
+$filename = '../../php/misc/poll_latest.csv';
 $fp = fopen($filename, "a+");
 if( !$fp ) {
 	echo "<h4> Thank you for your feedback </h4>";			
 	echo "<h4> But error occurred while saving-(Type_1) </h4>";
 } else {
-	if(fputcsv($fp, str_split($vote, strlen($vote)))) {
-		echo "<h4> Thank you for your feedback </h4>";		
-		echo "<h4> Successfully saved ! </h4>";
-		exit;
+	if($final == 0) {
+		if(fputcsv($fp, $list)) {
+			echo "<h4> Thank you for your feedback </h4>";		
+			echo "<h4> Successfully saved ! </h4>";
+			exit;
+		} else {
+			echo "<h4> Thank you for your feedback </h4>";		
+			echo "<h4> But error occurred while saving-(Type_2) </h4>";
+			exit;
+		}
 	} else {
-		echo "<h4> Thank you for your feedback </h4>";		
-		echo "<h4> But error occurred while saving-(Type_2) </h4>";
+		echo "<h4> Good attempt to corrupt our poll data </h4>";
+		echo "<h4> Sadly not saving ;) ! </h4>";
 		exit;
 	}	
+	fclose($fp);
 }
-fclose($fp);
 ?>
