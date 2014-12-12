47bfff436556e5bac5c474b51cb29f7f2ae0cdfb
Abdeali JK <abdealikothari@gmail.com>
2014-12-12 15:50:03 +0530

    Added psycho workshop

---
+++ projects/main/php/pages/accomodation.php
@@ -0,0 +1,61 @@
+<?php include '../../php/base/logmein.php'; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hospitality | Shaastra 2015</title>
+		<meta name="keywords" content="hospitality shaastra 2015,hospitality shaastra,shaastra hospi">
+        <meta name"description" content="We, the Hospitality team at Shaastra have pledged to leave no stone unturned to ensure that your stay with us is enjoyable and comfortable.">
+		<?php include '../../php/base/head.php' ?>
+		<style type="text/css">
+			.hospi_row > div {
+				margin: 1em;
+				/*border-bottom: 1px solid #fff;*/
+			}
+			.panel-heading a {
+				color: #000;
+			}
+			.panel-heading a:hover {
+				color: #555;
+				text-decoration: none;
+			}
+			.muted {
+				color: #888;
+				font-size: 80%;
+			}
+		</style>
+	</head>
+
+	<body>
+		<?php $back="../../php/pages/home.php"; include '../../php/base/menu.php' ?>
+		<div class="container-fluid white centered">
+	        <div class="row">
+	            <div class="col-xs-12">
+	                <h1 class="text-center title">Accomodation</h1>
+	                <div class="white breaker">
+	                    <span class="left"></span>
+	                        <div class="dice white"></div>
+	                    <span class="right"></span>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+
+		<div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <h3>Accomodation Calculator</h3>
+                    <br />
+                    For any accomodation related queries, please contact <a href='mailto:namit@shaastra.org'>namit@shaastra.org</a>
+                    <br />
+                </div>
+            </div>
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 text-justify">
+                    Test
+                </div>
+            </div>
+		</div>
+		<?php include '../../php/base/foot.php' ?>
+
+	</body>
+</html>
