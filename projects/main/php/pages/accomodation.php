7448065a8fe04bc9edf31ed2b1f7b5be74d3cd93
Abdeali JK <abdealikothari@gmail.com>
2014-12-13 22:04:50 +0530

    Added accom payment calculator

--- projects/main/php/pages/accomodation.php
+++ projects/main/php/pages/accomodation.php
@@ -50,12 +50,109 @@
                 </div>
             </div>
 			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2 text-justify">
-                    Test
+                <div class="row row-centered error-msg" style="padding: 1em; display: none;">
+                    <div class="col-md-6 text-center col-centered">
+                        <div class="alert alert-danger">
+                            <span class="bold">Error </span>
+                            <span class="text">There was an error</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-sm-offset-3 text-justify">
+                    <form role="form">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <label for="email" class="col-md-6 text-right">Number of boys</label>
+                                <div class="col-md-6 input-group">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-user"></span>
+                                    </span>
+                                    <input type="number" min="0" name="boys" class="form-control" id="boys" placeholder="" value="0">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="email" class="col-md-6 text-right">Number of girls</label>
+                                <div class="col-md-6 input-group">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-user"></span>
+                                    </span>
+                                    <input type="number" min="0" name="girls" class="form-control" id="girls" placeholder="" value="0">
+                                </div>
+                            </div>
+                        </div>
+                        <br /><br class="hiden-sm hidden-xs" /><br class="hiden-xs hidden-sm" />
+                        <div class="form-group">
+                            <label for="days" class="col-md-4 text-right col-md-offset-2">Number of days to book for</label>
+                            <div class="col-md-2 input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-calendar"></span>
+                                </span>
+                                <input type="number" min="1" class="form-control" name="days" id="days" placeholder="" value="1">
+                            </div>
+                        </div>
+                        <br /><br class="hiden-sm hidden-xs" /><br class="hiden-xs hidden-sm" />
+                        <div class="form-group disabled">
+                            <label for="cost" class="col-md-2 col-md-offset-4 text-right">Final Cost</label>
+                            <div class="col-md-2">
+                                <input class="form-control" type="text" id="cost" name="cost" value="0" disabled>
+                            </div>
+                        </div>
+                        <br />
+                        <div class="white" style="text-align: center">
+                            <span id="help" class=""></span>
+                        </div>
+                        <!-- <br /><br class="hiden-sm hidden-xs" /><br class="hiden-xs hidden-sm" />
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </div> -->
+                    </form>
                 </div>
             </div>
 		</div>
 		<?php include '../../php/base/foot.php' ?>
-
+        <script>
+            function calc() {
+                var boys = parseInt($('#boys').val());
+                var girls = parseInt($('#girls').val());
+                var people = boys + girls;
+                var days = parseInt($('#days').val());
+                var caution_deposit = 0
+                if ( boys < 0 || girls < 0 || people < 0 ) {
+                    $('.error-msg').show()
+                        .find(".text").html("We got invalid data. <br /> The number of people in your team cannot be negative")
+                    $('#cost').val("ERROR")
+                    return 0
+                } else if ( people == 0 ) {
+                    caution_deposit = 0
+                } else if ( people == 1 )
+                    caution_deposit = 700
+                else if ( people <= 3)
+                    caution_deposit = 1400
+                else if ( people <= 5 )
+                    caution_deposit = 2100
+                else {
+                    $('.error-msg').show()
+                        .find(".text").html("You can register for 5 people at a time. <br />In case your team consists of more than 5 people, please complete payment for 5 people and do this process once again for the remaining.")
+                    $('#cost').val("ERROR")
+                    $("#help").val("Only 5 people at a time !")
+                    return 1
+                }
+                var cost = people * days * 250 + 200 * people + caution_deposit
+                if ( people == 0 ) {
+                    $("#cost").val("0")
+                    $("#help").html("")
+                } else {
+                    $("#cost").val(cost.toString())
+                    $("#help").html("Caution Deposit : Rs. <u>" + caution_deposit.toString() + "</u> - <b>This will be returned to you when you leave</b>")
+                }
+                console.log(cost)
+                return 0
+            }
+            $(document).ready(function() {
+                $("#boys").change(calc)
+                $("#girls").change(calc)
+                $("#days").change(calc)
+            })
+        </script>
 	</body>
 </html>
